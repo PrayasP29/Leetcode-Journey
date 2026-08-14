0948e627ed7e327772932d2553c93695d84b57fe
Prayas Priyadarshi <prayaspriyadarshi18@gmail.com>
2026-08-14 21:02:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (15.61%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        for(char c:word){
+            if(isupper(c)){
+                count++;
+            }
+        }
+        if(count==0 || count==word.length() || count==1&&isupper(word[0])){
+            return true;
+        }
+        return false;
+    }
+};
