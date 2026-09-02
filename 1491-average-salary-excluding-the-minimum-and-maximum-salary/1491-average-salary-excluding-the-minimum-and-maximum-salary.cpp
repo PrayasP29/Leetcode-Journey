819b6986067e94a96628f2931d37a58184aaa006
Prayas Priyadarshi <prayaspriyadarshi18@gmail.com>
2026-09-02 20:35:05 +0530

    Time: 2 ms (0.94%), Space: 9.7 MB (49.44%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        sort(salary.begin(),salary.end());
+        int count=0;
+        int total=0;
+        double avg;
+        for(int i=1;i<salary.size()-1;i++){
+            total+=salary[i];
+            count++;
+            avg=(double)total/count;
+        }
+        return avg;
+    }
+};
