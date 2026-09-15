69df94adbf993fbbb4d638c1e8f1a5f8276a37a8
Prayas Priyadarshi <prayaspriyadarshi18@gmail.com>
2026-09-15 15:38:22 +0530

    Time: 0 ms (100%), Space: 20.7 MB (53.68%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int target=nums.size()-1;
         int count=0;
-        while(target!=0){
-            for(int i=0;i<target;i++){
-                if(i+nums[i]>=target){
-                    target=i;
-                    count++;
-                    break;
-                }
+        int end=0;
+        int far=0;
+
+        for(int i=0;i<nums.size()-1;i++){
+            far=max(i+nums[i],far);
+
+            if(i==end){
+                count++;
+                end=far;
             }
         }
         return count;
