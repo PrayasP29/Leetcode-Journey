f479f893b620d2fbc146419673bdb0edde97b1ab
Prayas Priyadarshi <prayaspriyadarshi18@gmail.com>
2026-09-25 18:03:18 +0530

    Time: 2 ms (77.91%), Space: 29.4 MB (96.39%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.cpp
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
     vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
         vector<int> ans;
-        unordered_set<int> s1(nums1.begin(),nums1.end());
-        unordered_set<int> s2(nums2.begin(),nums2.end());
-        unordered_set<int> s3(nums3.begin(),nums3.end());
-
-        unordered_set<int> val;
-
-        for(int x:nums1) val.insert(x);
-        for(int x:nums2) val.insert(x);
-        for(int x:nums3) val.insert(x);
-
-        for(int x:val){
-            int count =0;
-
-            if(s1.count(x)) count++;
-            if(s2.count(x)) count++;
-            if(s3.count(x)) count++;
-
-            if(count>=2){
+        for(int x:nums1){
+            if((find(nums2.begin(),nums2.end(),x)!=nums2.end()||
+            find(nums3.begin(),nums3.end(),x)!=nums3.end())&&
+            find(ans.begin(),ans.end(),x)==ans.end()){
+                ans.push_back(x);
+            }
+        }
+         for(int x:nums2){
+            if((find(nums1.begin(),nums1.end(),x)!=nums1.end()||
+            find(nums3.begin(),nums3.end(),x)!=nums3.end())&&
+            find(ans.begin(),ans.end(),x)==ans.end()){
                 ans.push_back(x);
             }
         }
